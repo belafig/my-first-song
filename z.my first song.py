6ab63f21b5cea238afdf8232de8a347e3912bd5b
belafig <40066180+belafig@users.noreply.github.com>
2020-06-02 14:03:15 -0300

    Add files via upload

---
+++ z.my first song.py
@@ -0,0 +1,6 @@
+p1 >> sitar ([1,5,7], dur = [1/2, 1, 1/2 ]).stop()
+p2 >> bass ([1], dur = [1/2])
+
+d1 >> play ("x-o-").stop()
+d2 >> play ("---(-=)", pan=0.5).stop()
+d3 >> play("x-(oo-(x-))").stop()
